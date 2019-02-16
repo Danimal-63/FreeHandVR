561020f2616e6cad54bdd3d9b2722172f9d01720
BitsByWill <8272056+BitsByWill@users.noreply.github.com>
2019-02-16 13:30:03 -0800

    xytrack

---
+++ trackingxy/xytrack.py
@@ -0,0 +1,114 @@
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import cv2
+import imutils
+import time
+import socket
+import sys
+import getopt
+
+## sending all 2d coordinates, Unity dealing with averages
+
+IP = '127.0.0.1'
+
+def socketSend(UDP_PORT):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.sendto(("JUMP!").encode(), (IP, UDP_PORT))
+
+if __name__ == '__main__':
+	camera, port = None, None
+	debug = 0
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], 'v:p:d', ['videoSrc=', 'port=', 'debug'])
+	except getopt.GetoptError:
+		exit(1)
+	for opt, arg in opts:
+		if opt in ('-v', '--videoSrc'):
+			camera = arg
+		if opt in ('-p', '--port'):
+			port = arg
+		if opt in ('-d', '--debug'):
+			debug = 1
+	## optimized HSV values for red and blue
+	redLower = (170, 150, 60)
+	blueLower = (90, 170, 60)
+	upper = (255, 255, 255)
+	videoSrc = VideoStream(src=int(camera)).start()
+	## camera warm up for 2 seconds
+	time.sleep(2.0)
+	## constant looping, no exit allowed, must be terminated by unity
+	while True:
+		# grab the current frame
+		frame = videoSrc.read()
+ 
+		# resize the frame, blur it, and convert it to the HSV color space
+		frame = imutils.resize(frame, width=600)
+		blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+ 
+		# construct a mask for the color "red", then perform and then erode and dilate to make smoother
+		mask = cv2.inRange(hsv, redLower, upper)
+		mask = cv2.erode(mask, None, iterations=2)
+		mask = cv2.dilate(mask, None, iterations=2)
+		# find contours in the mask and initialize the current and initialize (x, y) center of the ball
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)
+		cnts = imutils.grab_contours(cnts)
+		center = None
+ 
+		# only proceed if at least one contour was found
+		if len(cnts) > 0:
+			# find the largest contour in the mask, then use
+			# it to compute the minimum enclosing circle and
+			# centroid
+			c = max(cnts, key=cv2.contourArea)
+			((redX, redY), radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+ 
+		# only proceed if the radius meets a minimum size
+			if radius > 12:
+				# draw the circle and centroid on the frame,
+				# then update the list of tracked points
+				cv2.circle(frame, (int(redX), int(redY)), int(radius),
+					(0, 0, 255), 2)
+				cv2.circle(frame, center, 5, (255, 255, 255), -1)
+				strRed = 'Red (x, y): ' + str(int(redX)) + ', ' + str(int(redY))
+				if debug is 1:
+					print strRed
+		## now for blue
+		# construct a mask for the color "blue", then perform and then erode and dilate to make smoother
+		mask = cv2.inRange(hsv, blueLower, upper)
+		mask = cv2.erode(mask, None, iterations=2)
+		mask = cv2.dilate(mask, None, iterations=2)
+		# find contours in the mask and initialize the current and initialize (x, y) center of the ball
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)
+		cnts = imutils.grab_contours(cnts)
+		center = None
+ 
+		# only proceed if at least one contour was found
+		if len(cnts) > 0:
+			# find the largest contour in the mask, then use
+			# it to compute the minimum enclosing circle and
+			# centroid
+			c = max(cnts, key=cv2.contourArea)
+			((blueX, blueY), radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+ 
+		# only proceed if the radius meets a minimum size
+			if radius > 12:
+				# draw the circle and centroid on the frame,
+				# then update the list of tracked points
+				cv2.circle(frame, (int(blueX), int(blueY)), int(radius),
+					(255, 0, 0), 2)
+				cv2.circle(frame, center, 5, (255, 255, 255), -1)
+				strBlue = 'Blue (x, y): ' + str(int(blueX)) + ', ' + str(int(blueY))
+				if debug is 1:
+					print strBlue
+		# show the frame to our screen
+		if debug is 1:
+			cv2.imshow("Frame", frame)
+			key = cv2.waitKey(1) & 0xFF
