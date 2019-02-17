79878803a145ca28da8093bbc2c249e42900b116
BitsByWill <8272056+BitsByWill@users.noreply.github.com>
2019-02-17 08:14:09 -0800

    fix camera detect

--- camera_detectors/which_cameras.py
+++ camera_detectors/which_cameras.py
@@ -7,19 +7,21 @@ import imutils
 videoSrc = [None] * 20
 frame = [None] * 20
 name = [None] * 20
+i = 0
 for i in range(20):
 	try:
-		videoSrc[i] = VideoStream(src=i).start
+		videoSrc[i] = VideoStream(src=i).start()
+		i=+1
 	except:
-		break:
-for i in range(len(videoSrc)):
-	name[i] = "Camera" + str(i)
+		break
+for i in range(i):
+	name[i] = "Camera " + str(i)
 while True:
-	for i in range(len(videoSrc)):
-		frame[i] = videoSrc[i].read()
-		frame[i] = imutils.resize(frame, width=600)
-	for i in range(len(videoSrc)):
-		cv2.imshow(name[i], frame)
+	for j in range(i + 1):
+		frame[j] = videoSrc[j].read()
+		frame[j] = imutils.resize(frame[j], width=600)
+	for j in range(i + 1):
+		cv2.imshow(name[j], frame[j])
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 cv2.destroyAllWindows()
