074a64f506be89332f52ada1d1d79191468088ac
Yvette <yjcopeland@gmail.com>
2019-02-17 08:48:45 -0800

    cam stuff finally works

--- camera_detectors/which_cameras.py
+++ camera_detectors/which_cameras.py
@@ -4,24 +4,30 @@ from imutils.video import VideoStream
 import cv2
 import imutils
 
-videoSrc = [None] * 20
-frame = [None] * 20
-name = [None] * 20
-i = 0
-for i in range(20):
-	try:
-		videoSrc[i] = VideoStream(src=int(i)).start()
-		i=+1
-	except:
-		break
-for j in range(i + 1):
-	name[j] = "Camera " + str(j)
-print name
+index = 0
+arr = []
 while True:
-	for j in range(i + 1):
+    cap = cv2.VideoCapture(index)
+    if not cap.read()[0]:
+        break
+    else:
+        arr.append(index)
+    cap.release()
+    index += 1
+
+videoSrc = []
+
+for j in range(len(arr)):
+		videoSrc.append(VideoStream(src=int(arr[j])).start())
+name = []
+for j in range(len(arr)):
+	name.append("Camera " + str(arr[j]))
+frame = [None] * len(arr)
+while True:
+	for j in range(len(arr)):
 		frame[j] = videoSrc[j].read()
 		frame[j] = imutils.resize(frame[j], width=600)
-	for j in range(i + 1):
+	for j in range(len(arr)):
 		cv2.imshow(name[j], frame[j])
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
